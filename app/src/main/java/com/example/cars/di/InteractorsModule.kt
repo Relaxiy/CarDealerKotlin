cad5f1357ea2688b62622828e9e85cda923fa00f
Relaxiy <terer435461@gmail.com>
2022-03-23 18:38:42 +0300

    Cars

--- app/src/main/java/com/example/cars/di/InteractorsModule.kt
+++ app/src/main/java/com/example/cars/di/InteractorsModule.kt
@@ -1,12 +1,6 @@
 package com.example.cars.di
 
-import com.example.cars.app.data.network.api.CarApi
-import com.example.cars.app.domain.interactors.CarInteractorImpl
-import com.example.cars.registration.data.room.dao.AccountsDao
-import com.example.cars.registration.domain.interactor.AccountsInteractorImpl
 import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
 
 @Module(
     includes = [
--- app/src/main/java/com/example/cars/registration/domain/interactor/AccountsInteractorImpl.kt
+++ app/src/main/java/com/example/cars/registration/domain/interactor/AccountsInteractorImpl.kt
@@ -8,40 +8,52 @@ import com.example.cars.registration.data.room.models.AccountDbEntity
 import com.example.cars.registration.domain.models.SignUpData
 import com.example.cars.registration.data.room.tuples.AccountUpdateUsernameTuple
 import com.example.cars.registration.domain.models.AccountSignIn
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
-class AccountsInteractorImpl @Inject constructor(private val accountsDao: AccountsDao) : AccountsInteractor {
+class AccountsInteractorImpl @Inject constructor(private val accountsDao: AccountsDao) :
+    AccountsInteractor {
 
     override suspend fun findAccountIdByEmailAndPassword(accountSignIn: AccountSignIn): Long {
-        val tuple = accountsDao.findByEmail(accountSignIn.email) ?: throw AuthenticatorException()
-        if (tuple.password != accountSignIn.password) throw AuthenticatorException()
-        return tuple.id
+        return withContext(Dispatchers.IO) {
+            val tuple =
+                accountsDao.findByEmail(accountSignIn.email) ?: throw AuthenticatorException()
+            if (tuple.password != accountSignIn.password) throw AuthenticatorException()
+            return@withContext tuple.id
+        }
     }
 
     override suspend fun createAccount(signUpData: SignUpData) {
-        try {
-            val entity = AccountDbEntity.fromSignUpData(signUpData)
-            accountsDao.createAccount(entity)
-        } catch (e: SQLiteConstraintException) {
+        withContext(Dispatchers.IO) {
+            try {
+                val entity = AccountDbEntity.fromSignUpData(signUpData)
+                accountsDao.createAccount(entity)
+            } catch (e: SQLiteConstraintException) {
+            }
         }
     }
 
     override suspend fun getAccountById(accountId: Long): Flow<Account?> {
-        return accountsDao.getAccountById(accountId).map { accountDbEntity ->
-            accountDbEntity?.toAccount()
+        return withContext(Dispatchers.IO) {
+            return@withContext accountsDao.getAccountById(accountId).map { accountDbEntity ->
+                accountDbEntity?.toAccount()
+            }
         }
     }
 
     override suspend fun updateUsernameForAccountId(accountId: Long, newUsername: String) {
-        accountsDao.updateUsername(
-            AccountUpdateUsernameTuple(
-                accountId,
-                newUsername
+        withContext(Dispatchers.IO) {
+            accountsDao.updateUsername(
+                AccountUpdateUsernameTuple(
+                    accountId,
+                    newUsername
+                )
             )
-        )
+        }
     }
 
 
